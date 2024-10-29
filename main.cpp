e3ee7a4ddbca6a98e40553eb87b2e538787ed354
Sorin Ocneanu <sorinocneanu@gmail.com>
2024-10-30 00:41:28 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+
+struct Problema{
+    string id_problem;
+    string id_specialty;
+};
+
+
+struct Doctor{
+ string name;
+ string speciality;
+
+};
+
+
+
 int main()
 {
+    vector<Doctor> Doc;
+    vector<Problema> Pro;
+
     ifstream inFile("input.txt");
 
     int no_problems, no_doctors;
+
     string name, speciality;
-    
     inFile >> no_problems;
 
     for (int i = 0; i < no_problems; i++)
     {
-        inFile >> name;
-        inFile >> speciality;
+        inFile >> Pro[i].id_problem;
+        inFile >> Pro[i].id_specialty;
         cout << name << ' ' << speciality << '\n';
     }
 
@@ -24,10 +43,19 @@ int main()
 
     for (int i = 0; i < no_doctors; i++)
     {
-        inFile >> name;
-        inFile >> speciality;
+        inFile >> Doc[i].name;
+        inFile >> Doc[i].speciality;
         cout << name << ' ' << speciality << '\n';
     }
+
+    for(int i = 0; i < no_problems; i++)
+    {
+        for (int i = 0; i< no_doctors;i++)
+        {
+
+        }
+    }
+
 
     return 0;
 }
